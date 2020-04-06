7dc5587939a85b9ae4573ac45cfbf0f7d4a6a938
rxtur <rtur.net@gmail.com>
2020-04-05 19:23:35 -0500

    Initial check-in

---
+++ src/Blogifier.Core/Data/Domain/CustomField.cs
@@ -0,0 +1,10 @@
+﻿namespace Blogifier.Core.Data
+{
+    public class CustomField
+    {
+        public int Id { get; set; }
+        public int AuthorId { get; set; }
+        public string Name { get; set; }
+        public string Content { get; set; }
+    }
+}
