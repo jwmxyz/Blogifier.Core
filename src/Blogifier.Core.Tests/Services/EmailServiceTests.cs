f33f6671c0b6e99fe285b12ea3fa6e358d339729
rxtur <rtur.net@gmail.com>
2020-04-13 19:54:49 -0500

    Email testing updates

--- src/Blogifier.Core.Tests/Services/EmailServiceTests.cs
+++ src/Blogifier.Core.Tests/Services/EmailServiceTests.cs
@@ -1,7 +1,11 @@
-﻿using Blogifier.Core.Services;
+﻿using Blogifier.Core.Data;
+using Blogifier.Core.Services;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Moq;
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -9,12 +13,36 @@ namespace Core.Tests.Services
 {
     public class EmailServiceTests
     {
+        private readonly string email = "test@test.com";
+
         [Fact]
-        public async Task CanImportFromRssFeed()
+        public async Task CanSendEmail()
         {
             var sut = GetSut();
 
-            await sut.SendEmail("rtur.net@gmail.com", "test", "testing blogifier");
+            await sut.SendEmail(email, "test", "testing blogifier");
+
+            Assert.NotNull(sut);
+        }
+
+        [Fact]
+        public async Task CanSendNewsletters()
+        {
+            var sut = GetSut();
+            var emails = new List<string> { email };
+
+            BlogPost post = new BlogPost
+            {
+                Title = "test",
+                Description = "test",
+                Content = "test",
+                Slug = "test",
+                Published = DateTime.Now.AddDays(-2),
+                Cover = "",
+                AuthorId = 1
+            };
+
+            await sut.SendNewsletters(post, emails, "http://blogifier.net");
 
             Assert.NotNull(sut);
         }
@@ -22,11 +50,24 @@ namespace Core.Tests.Services
         private SendGridService GetSut()
         {
             var data = new Mock<IDataService>();
+
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseSqlite("DataSource=Blog.db").Options;
+
+            var context = new AppDbContext(options);
+
+            IPostRepository posts = new PostRepository(context);
+            IAuthorRepository authors = new AuthorRepository(context);
+            INewsletterRepository letters = new NewsletterRepository(context);
+            ICustomFieldRepository custom = new CustomFieldRepository(context);
+
+            IDataService ds = new DataService(context, posts, authors, null, null, custom, letters);
+
             var logger = new Mock<ILogger<SendGridService>>();
             var storage = new Mock<IStorageService>();
             var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
 
-            return new SendGridService(data.Object, config, logger.Object, storage.Object);
+            return new SendGridService(ds, config, logger.Object, storage.Object);
         }
     }
 }
